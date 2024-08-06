d5ffed17691ebdf4ea0f5869fb35012b8590a271
MAllk33 <MAllk33@163.com>
2024-08-06 17:27:04 +0800

    修改配置与注释

--- src/main/java/javaprogramming/commonmistakes/common/Utils.java
+++ src/main/java/javaprogramming/commonmistakes/common/Utils.java
@@ -7,13 +7,20 @@ import java.util.Properties;
 @Slf4j
 public class Utils {
 
-    public static void loadPropertySource(Class clazz,String fileName){
+    /**
+     * 加载指定类路径下的属性文件，并将属性设置为系统属性，同时在日志中输出每个属性的键值对。
+     *
+     * @param clazz    类，用于定位属性文件的类路径。
+     * @param fileName 属性文件的名称。
+     */
+    public static void loadPropertySource(Class clazz, String fileName) {
         try {
-            Properties p=new Properties();
+            Properties p = new Properties();
+            // 加载指定类路径下的属性文件
             p.load(clazz.getResourceAsStream(fileName));
-            p.forEach((k,v)->{
-                log.info("{}={}",k,v);
-                System.setProperty(k.toString(),v.toString());
+            p.forEach((k, v) -> {
+                log.info("{}={}", k, v);
+                System.setProperty(k.toString(), v.toString());
             });
         } catch (Exception e) {
             throw new RuntimeException(e);
